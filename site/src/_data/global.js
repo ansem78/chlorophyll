8157b7ccde6dd4361b5834ddcb2c839eb0c20b27
Alexandro <amorato@acknow.it>
2021-02-01 12:56:03 +0100

    Improve documentation

--- site/src/_data/global.js
+++ site/src/_data/global.js
@@ -1,64 +1,60 @@
+const fs = require('fs');
 const package = require('../../../package.json');
 
+const upperCaseFirst = (str) => {
+  return str.substr(0, 1).toUpperCase() + str.substr(1);
+};
+
+const loadFeatureVars = (feature) => {
+  const vars = [];
+  try {
+
+    let data = fs.readFileSync(`scss/features/${feature}/_variables.scss`, 'UTF8');
+    data = data.trim().replace(/[\t ]+/g, ' ');
+    data = data.replace(/: \([\r\n]+/g, ': ( ');
+    data = data.replace(/,[\r\n]+/g, ', ');
+    data = data.replace(/[\r\n]+\);/g, ' );');
+
+    const lines = data.split(/\r?\n\r?\n/);
+
+    lines.forEach(line => {
+      line = line.replace(/;$/, '');
+      line = line.replace(/: /, '\n');
+      const tokens = line.split('\n');
+      vars.push({
+        name: tokens[0],
+        value: tokens[1],
+        featureId: feature,
+        featureName: upperCaseFirst(feature)
+      });
+    });
+
+  }
+  catch (err) {
+    console.error(err);
+  }
+  return vars;
+};
+
 module.exports = {
-  name: package.name.substr(0, 1).toUpperCase() + package.name.substr(1),
+  id: package.name,
+  name: upperCaseFirst(package.name),
   version: package.version,
   description: package.description,
   license: package.license,
   author: package.author,
   repository: package.repository.url.replace(/\.?git\+?/g, ''),
   bug: package.bugs.url,
-  features: [
-    {
-      name: 'Core',
-      modules: []
-    },
-    {
-      name: 'Debug',
-      modules: [
-        'Debug',
-        'Debug utilities'
-      ]
-    },
-    {
-      name: 'Animate',
-      modules: [
-        'Attention seeker',
-        'Back',
-        'Bounce',
-        'Fade',
-        'Flip',
-        'Rotate',
-        'Slide',
-        'Special',
-        'Speed',
-        'Zoom'
-      ]
-    },
-    {
-      name: 'Embed',
-      modules: []
-    },
-    {
-      name: 'Icon',
-      modules: []
-    },
-    {
-      name: 'Reset',
-      modules: []
-    },
-    {
-      name: 'Sizing',
-      modules: []
-    },
-    {
-      name: 'Transform',
-      modules: [
-        'Flip',
-        'Rotate'
-      ]
-    }
-  ],
+  features: {
+    core: loadFeatureVars('core'),
+    debug: loadFeatureVars('debug'),
+    animate: loadFeatureVars('animate'),
+    embed: loadFeatureVars('embed'),
+    icon: loadFeatureVars('icon'),
+    reset: loadFeatureVars('reset'),
+    sizing: loadFeatureVars('sizing'),
+    transform: loadFeatureVars('transform')
+  },
   utils: [
     'Assert',
     'Color',
